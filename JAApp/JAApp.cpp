8f12ba082e62001c78dcccc9a5d5a6035a107940
Krzysztof Werner <krzysztof.werner@infotower.pl>
2020-01-15 15:09:41 +0100

    Preparing to save image from rgb values

--- JAApp/JAApp.cpp
+++ JAApp/JAApp.cpp
@@ -27,7 +27,9 @@ int main() {
 	struct jpeg_decompress_struct cinfo;
 	struct jpeg_error_mgr jerr;
 	const char * filePath;
-	filePath = "C:\\Users\\admin\\Documents\\ja\\jpeg-9b\\testimg.jpg";
+	const char * filePathOut;
+	filePath = "C:\\Users\\User\\Documents\\studia\\ja\\jpeg-build\\jpeg-6b\\testoutt.jpg";
+	filePathOut = "C:\\Users\\User\\Documents\\studia\\ja\\jpeg-build\\jpeg-6b\\testout.jpg";
 	FILE * infile;        /* source file */
 	JSAMPARRAY pJpegBuffer;       /* Output row buffer */
 	int row_stride;       /* physical row width in output buffer */
@@ -90,11 +92,68 @@ int main() {
 
 
 
-
+	save_jpg_to_file(filePathOut, width, height);
 
 	int k = 3, w = 4, c = 0;
 	c = MyProc1(k, w); // wywo³anie procedury asemblerowej z biblioteki
 	std::cout << c << "\n";
 
 	return 0;
+}
+
+bool save_jpg_to_file(const char *file_name, int width, int height) {
+	struct jpeg_compress_struct cinfo;
+
+	struct jpeg_error_mgr jerr;
+
+	FILE * outfile;
+	JSAMPROW row_pointer[1];
+	int row;
+
+	JSAMPLE * image_buffer;
+
+	cinfo.err = jpeg_std_error(&jerr);
+	jpeg_create_compress(&cinfo);
+
+	if ((outfile = fopen(file_name, "wb")) == NULL) {
+		fprintf(stderr, "can't open %s\n", file_name);
+		return false;
+	}
+
+	jpeg_stdio_dest(&cinfo, outfile);
+
+
+	cinfo.image_width = width;
+	cinfo.image_height = height;
+	cinfo.input_components = 3;
+	cinfo.in_color_space = JCS_RGB;
+
+	jpeg_set_defaults(&cinfo);
+	jpeg_set_quality(&cinfo, 100, TRUE);
+
+	jpeg_start_compress(&cinfo, TRUE);
+
+	unsigned char bytes[3000]; //tutaj wielkosc
+
+	while (cinfo.next_scanline < cinfo.image_height) {
+		for (int i = 0; i < width; i += 3){
+			
+
+			//tutaj dodaæ moje
+
+			bytes[i] = static_cast<int>(colorData.red() * 255) & 0xff;
+			bytes[i + 1] = static_cast<int>(colorData.green() * 255) & 0xff;
+			bytes[i + 2] = static_cast<int>(colorData.blue() * 255) & 0xff;
+		}
+		row_pointer[0] = bytes;
+		(void)jpeg_write_scanlines(&cinfo, row_pointer, 1);
+	}
+
+	jpeg_finish_compress(&cinfo);
+	fclose(outfile);
+	jpeg_destroy_compress(&cinfo);
+
+	std::cout << "Done" << std::endl;
+
+	return true;
 }
